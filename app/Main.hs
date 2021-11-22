0c8189e5f15aead0833f4c3d6bd90afaeb67db8a
Sarah Ekaireb <sekaireb@ucsd.edu>
2021-11-21 22:25:16 -0800

    added exit, win/lose logic, lock

--- app/Main.hs
+++ app/Main.hs
@@ -36,19 +36,35 @@ data Game = Game
     , _hwalls    :: [Coord]
     , _bsize    :: Int
     , _exit     :: Coord
+    , _lock    :: Bool
+    , _gameState :: GameState
     }
 
 test :: Game
 test = Game { _explorer = Coord 1 1
                  , _mummies  = [(Mummy {_mloc = (Coord 6 6), _mct = 2, _mrct = 0}), (Mummy {_mloc = (Coord 8 3), _mct = 2, _mrct = 0})]
                  , _vwalls    = [(Coord 2 3), (Coord 2 4), (Coord 6 6), (Coord 8 2), (Coord 8 3), (Coord 8 4)]
-                 , _hwalls    = [(Coord 2 3), (Coord 3 3), (Coord 6 6), (Coord 7 6)]
+                 , _hwalls    = [(Coord 3 0), (Coord 2 3), (Coord 3 3), (Coord 6 6), (Coord 7 6)]
                  , _bsize    = 10
-                 , _exit     = Coord 1 1
+                 , _exit     = Coord 3 0
+                 , _lock     = True
+                 , _gameState = Playing
+                 }
+
+level_1 :: Game
+level_1 = Game { _explorer = Coord 1 1
+                 , _mummies  = [(Mummy {_mloc = (Coord 0 4), _mct = 2, _mrct = 0})]
+                 , _vwalls    = [(Coord 0 3), (Coord 2 1), (Coord 4 1), (Coord 1 3), (Coord 3 5)]
+                 , _hwalls    = []
+                 , _bsize    = 6
+                 , _exit     = Coord 3 0
+                 , _lock     = True
+                 , _gameState = Playing
                  }
 
 data Coord = Coord { x :: Int, y :: Int} deriving (Eq, Ord, Show)
 
+data GameState = Win | Lose | Playing
 
 data Mummy = Mummy
     { _mloc :: Coord
@@ -65,7 +81,7 @@ app = App { appDraw = drawUI
           }
 
 handleEvent :: Game -> BrickEvent Name Counter -> EventM Name (Next Game)
-handleEvent g (AppEvent Counter)                    = continue $ moveMummies g
+handleEvent g (AppEvent Counter)                    = continue $ counterStep g
 handleEvent g (VtyEvent (V.EvKey (V.KChar 'q') [])) = halt g
 handleEvent g (VtyEvent (V.EvKey V.KEsc []))        = halt g
 handleEvent g (VtyEvent (V.EvKey V.KUp []))         = continue $ moveExplorer North g
@@ -85,14 +101,24 @@ data MyDirection
   | West
   deriving (Eq, Show)
 
+-- Special event function
+counterStep :: Game -> Game
+counterStep g = case (_gameState g) of
+    Playing -> moveMummies g 
+    _ -> g {_lock = False}
+
 -- Moves the mummies
 moveMummies :: Game -> Game
-moveMummies g = g {_mummies = (map (moveMummy ec g) mummies)}
+moveMummies g | movesLeft == 0   = g {_mummies = (map (moveMummy ec g) mummies), _lock = True, _gameState = gameState}
+              | otherwise        = g {_mummies = (map (moveMummy ec g) mummies), _gameState = gameState}
     where mummies = _mummies g
           ec = _explorer g
+          new_mummies = (map (moveMummy ec g) mummies)
+          movesLeft = foldr (\m b -> (_mrct m) + b) 0 new_mummies
+          gameState = checkGameState g
 
 -- Moves the mummy based on where the explorer is
-moveMummy :: Coord -> Game -> Mummy -> Mummy
+moveMummy :: Coord -> Game -> Mummy -> Mummy 
 moveMummy c@(Coord c1 c2) g m | mrct <= 0 = m
                               | otherwise = m {_mloc = new_coord, _mrct = (mrct-1)}
     where
@@ -107,12 +133,29 @@ moveMummy c@(Coord c1 c2) g m | mrct <= 0 = m
         | d2 > 0 && not((validMove South mloc g)==mloc) = (validMove South mloc g)
         | otherwise  = mloc
 
+-- Checks if mummy caught explorer, and returns if game is over
+checkGameState :: Game -> GameState
+checkGameState g = case (elem (_explorer g) (map (\m -> (_mloc m)) (_mummies g))) of
+    True -> Lose
+    False -> Playing
+
+
 -- Moves the explorer based on direction
 moveExplorer :: MyDirection -> Game -> Game
-moveExplorer d g = g {_explorer = new_coords, _mummies = setMummyCounters g }
+moveExplorer d g | _lock g   = g {_explorer = new_coords, _mummies = setMummyCounters g, _lock = False, _gameState = gameState}
+                 | otherwise = g
     where 
         cur_coords@(Coord x y) = _explorer g
         new_coords = validMove d cur_coords g
+        new_game = g {_explorer = new_coords}
+        gameState = checkWin new_game
+
+
+-- Checks if explorer won the game
+checkWin :: Game -> GameState
+checkWin g = case ((_explorer g) == (_exit g)) of
+    True -> Win
+    False -> Playing
 
 -- Sets the mummy move counters
 setMummyCounters :: Game -> [Mummy]
@@ -139,7 +182,15 @@ validMove d c@(Coord x y) g | d == North && not (elem (Coord x y) (_hwalls g)) &
 
 -- draw the game UI
 drawUI :: Game -> [Widget Name]
-drawUI g = (drawCharacters g) ++ [(drawGrid g)]
+drawUI g = case (_gameState g) of
+    Playing -> (drawCharacters g) ++ [(drawGrid g)]
+    Lose -> [(drawGrid g)]
+    Win -> (drawCharacters g)
+{-
+drawUI :: Game -> [Widget Name]
+drawUI g | (_gameState g) == Playing   = (drawCharacters g) ++ [(drawGrid g)]
+         | otherwise          = [(drawGrid g)]
+-}
 
 -- draws all of the characters
 drawCharacters :: Game -> [Widget Name]
@@ -174,7 +225,7 @@ drawGrid g = vBox rows
       | mod (x + y) 2 == 0  = Empty0  
       | otherwise           = Empty1
     drawWall walls c@(Coord c1 c2)
-      | elem c walls       = drawCell Wall
+      | elem c walls      = drawCell Wall
       | otherwise          = drawCell NoWall
 
 -- Helper function for drawGrid
